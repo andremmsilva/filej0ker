02a6e011e935b410b4e99ae3133f0d96d771ac53
André Silva <andre.miguel.silva@tecnico.ulisboa.pt>
2024-06-10 21:35:23 +0100

    feat: Setup psql docker database (#6)

---
+++ data/01-init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    user_name VARCHAR(32) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_role VARCHAR(50) NOT NULL,
+    active BOOLEAN DEFAULT FALSE
+)
