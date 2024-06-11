745f592c9ad8044632ab37398fc0940e091f3c8d
André Silva <andre.miguel.silva@tecnico.ulisboa.pt>
2024-06-11 16:35:06 +0100

    feat: Added clean shutdown on SIGTERM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import 'dotenv/config';
 import { createApp } from './createApp';
+import { pool } from './middleware/db';
 const PORT = process.env.EXPRESS_PORT;
 
-createApp().listen(PORT, () => {
+const server = createApp().listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(async () => {
+    await pool.end();
+    console.log('HTTP server was closed!');
+  })
+})
