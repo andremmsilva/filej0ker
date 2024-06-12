8a1c60f56b51aaed5685a0ef4b17b61258dd5efa
André Silva <andre.miguel.silva@tecnico.ulisboa.pt>
2024-06-12 17:33:43 +0100

    fix: Change declaration merging to the auth middleware file

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,16 +1,24 @@
 // middleware/auth.js
-import { NextFunction, Request, Response } from "express-serve-static-core";
-import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from 'express';
+import { JwtPayload, verify } from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userEmail?: string | JwtPayload;
+    }
+  }
+}
 
 export function authenticateToken(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const token = req.headers["authorization"];
+  const token = req.headers['authorization'];
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_SECRET!, (err, user) => {
+  verify(token, process.env.JWT_SECRET!, (err, user) => {
     if (err) return res.sendStatus(403);
     req.userEmail = user;
     next();
