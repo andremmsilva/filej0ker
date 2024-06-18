5a1b5aa307bc3cb184e2f5fbd182b1207accb54a
André Silva <andre.miguel.silva@tecnico.ulisboa.pt>
2024-06-18 17:52:01 +0100

    Setup light/dark themes

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,5 +7,25 @@
 </template>
 
 <script lang="ts" setup>
-  //
+import { onMounted, onUnmounted } from "vue";
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+const callback = (obj: { matches: boolean }) => {
+  theme.global.name.value = obj.matches ? "dark" : "light";
+};
+
+onMounted(() => {
+  callback({matches: window.matchMedia("(prefers-color-scheme: dark)").matches});
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", callback);
+});
+
+onUnmounted(() => {
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .removeEventListener("change", callback);
+});
 </script>
